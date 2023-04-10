526192171c11dd4481928426360e9e7aa7fa4244
Hamude Homsi <Hamude.Homsi@endava.com>
2023-04-10 18:31:43 +0300

    feat(dropdown-panel): use FloatingUI to render the panel

--- packages/bee-q/src/components/dropdown-panel/bq-dropdown-panel.tsx
+++ packages/bee-q/src/components/dropdown-panel/bq-dropdown-panel.tsx
@@ -1,4 +1,6 @@
-import { h, Component } from '@stencil/core';
+import { h, Component, State } from '@stencil/core';
+
+import { FloatingUI } from '../../services/libraries';
 
 @Component({
   tag: 'bq-dropdown-panel',
@@ -8,6 +10,9 @@ import { h, Component } from '@stencil/core';
 export class BqDropdownPanel {
   // Own Properties
   // ====================
+  private trigger: HTMLElement;
+  private panel: HTMLElement;
+  private floatingUI: FloatingUI;
 
   // Reference to host HTML element
   // ===================================
@@ -15,6 +20,8 @@ export class BqDropdownPanel {
   // State() variables
   // Inlined decorator, alphabetical order
   // =======================================
+
+  @State() private isVisible = false;
 
   // Public Property API
   // ========================
@@ -29,6 +36,20 @@ export class BqDropdownPanel {
   // Component lifecycle events
   // Ordered by their natural call order
   // =====================================
+
+  componentDidLoad() {
+    this.floatingUI = new FloatingUI(this.trigger, this.panel, {
+      placement: 'bottom',
+      distance: 0,
+      sameWidth: false,
+      strategy: 'fixed',
+      skidding: 0,
+    });
+  }
+
+  disconnectedCallback() {
+    this.floatingUI?.destroy();
+  }
 
   // Listeners
   // ==============
@@ -45,15 +66,31 @@ export class BqDropdownPanel {
   // These methods cannot be called from the host element.
   // =======================================================
 
+  private openPanel = (): void => {
+    this.isVisible = !this.isVisible;
+    this.floatingUI?.update();
+  };
+
   // render() function
   // Always the last one in the class.
   // ===================================
 
   render() {
     return (
-      <p class="m-[var(--bq-dropdown-panel--margin)]">
-        My name is Stencil <slot />
-      </p>
+      <div part="base">
+        <div ref={(el) => (this.trigger = el)} part="trigger" onClick={this.openPanel}>
+          <slot name="trigger" />
+        </div>
+        <div
+          aria-hidden={!this.isVisible}
+          hidden={!this.isVisible}
+          ref={(el) => (this.panel = el)}
+          role="list"
+          part="panel"
+        >
+          <slot name="panel" />
+        </div>
+      </div>
     );
   }
 }
--- packages/bee-q/src/components/dropdown-panel/readme.md
+++ packages/bee-q/src/components/dropdown-panel/readme.md
@@ -5,6 +5,15 @@
 <!-- Auto Generated Below -->
 
 
+## Shadow Parts
+
+| Part        | Description |
+| ----------- | ----------- |
+| `"base"`    |             |
+| `"panel"`   |             |
+| `"trigger"` |             |
+
+
 ----------------------------------------------
 
 *Built with [StencilJS](https://stenciljs.com/)*
